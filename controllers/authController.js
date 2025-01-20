c68ba9fa04faa4f33e95f3fb015b9ce74a50a52e
sarniva <sarnivadas@gmail.com>
2025-01-20 18:16:53 +0530

    code some controller

---
+++ controllers/authController.js
@@ -0,0 +1,32 @@
+const User = require("../models/User.js")
+const jwt = require('jsonwebtoken')
+
+const registerUser = async (req,res)=>{
+    const {username, email,password} = req.body
+    try {
+        const user = new User({username,email,password})
+        await user.save()
+        res.status(201).json({message:"User registered successfully"})
+    } catch (err) {
+        res.status(400).json({error : err.message})
+    }
+}
+
+
+const loginUser = async(req,res)=>{
+    const {email,password}= req.body
+    try {
+        const user = await User.findOne({email})
+        if(!user){
+            return res.status(400).json({error:"Invalid credential"})
+        }
+        const isMatch = await user.comparePassword(password)
+        if(!isMatch){
+            return res.status(400).json({error:"Invalid credential"})
+        }
+        const token = jwt.sign({id:user._id},process.env.JWT_SECRET,{expiresIn:'1h'})
+        res.json({token})
+    } catch (err) {
+        
+    }
+}
