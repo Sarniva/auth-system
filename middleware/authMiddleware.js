e62db0daa1bb6032b43b237a19d219d4c48bb0b3
sarniva <sarnivadas@gmail.com>
2025-01-21 13:43:46 +0530

    full middleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -5,7 +5,17 @@ const authMiddleware = (req,res,next)=>{
 
     if(!token) return res.status(400).json({error:"Access denied"});
     
+    try {
+        const decoded = jwt.verify(token,process.env.JWT_SECRET)
+        req.userId = decoded.id
+        next()
+
+    } catch (err) {
+        res.status(401).json({error:'Invalid Token'})
+    }
 
 
 
 }
+
+module.exports = authMiddleware
