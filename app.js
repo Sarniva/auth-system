e1e7b9a3ba1f48d89d12d6962aaa200be1a20b08
sarniva <sarnivadas@gmail.com>
2025-01-21 16:30:07 +0530

    change the inline import statement for more readability

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const cors = require('cors')
+const router = require('./routes/auth.js')
 
 const connectDB = require('./config/db.js')
 
@@ -14,7 +15,7 @@ const PORT = process.env.PORT || 3000
 app.use(cors())
 app.use(express.json())
 
-app.use('/api/auth',require('./routes/auth.js'))
+app.use('/api/auth',router)
 
 app.listen(PORT,()=>{
     console.log(`Server running on port ${PORT}`);
